437477d587cb403776a8e652edf262d33af122e9
nywilken <wrivera00@gmail.com>
2013-02-13 18:29:30 -0500

    Adding base RoR application for the Opsgadget blog. This is the generated code from running rails new Opsgadget.

---
+++ Opsgadget/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optsgadget::Application.config.secret_token = 'e0b00c8620406ded0f80bbef5f9d5cd2e4927a545d2b9a2b56dfbbf0507236641ab9539de0de22dfc4752f969e91a612d11651e07257df30a28503b663ca3db7'
---
+++ Opsgadget/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Optsgadget::Application.config.session_store :cookie_store, :key => '_optsgadget_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Optsgadget::Application.config.session_store :active_record_store
